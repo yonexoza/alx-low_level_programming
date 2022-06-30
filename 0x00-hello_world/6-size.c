53893333c94f96a3346beb80aedcf6f216a0a5ff
root <root@JONZI.JONZI>
2022-06-30 14:44:03 +0300

    prints the size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,10 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long d;
-float e;
-
-printf("size of a char: %d byte(s)\n", sizeof(a));
-printf("size of an int: %d  byte(s)\n", sizeof(b));
-printf("size of a long int: %d byte(s)\n", sizeof(c));
-printf("size of a long long int: %d byte(s)\n", sizeof(d));
-printf("size of a float: %d byte(s)\n", sizeof(e);
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of a float: %d byte(s)\n", sizeof(long long int));
 return (0);
 }
