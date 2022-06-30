27b5e98648731597abcd08a27a9ab18ca64404a7
root <root@JONZI.JONZI>
2022-06-30 15:05:44 +0300

    generates the assembly code (Intel syntax) of a C code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 /**
  * main - entry point
- * return: always 0
+ * Return: always 0
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
