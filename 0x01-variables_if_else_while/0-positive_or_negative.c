e462327f65f699ab52673869238dc8298e579600
root <root@JONZI.JONZI>
2022-07-01 23:54:53 +0300

    variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if (n > 0)
@@ -18,6 +19,6 @@ if (n == 0)
 if (n < 0)
 	{
 	printf("%d is negative\n", n);
-	}	
+	}
 return (0);
 }
