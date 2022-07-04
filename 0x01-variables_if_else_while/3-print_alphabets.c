3f203915c280ff0ab4874ee3198f0b9dff6a9a24
root <root@JONZI.JONZI>
2022-07-04 22:21:34 +0300

    prints the alphabet in lowercase & uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 65; n <= 90; n++)
+	{
+		putchar(n);
+	}
+	Putchar(10);
+
+return (0);
+}
