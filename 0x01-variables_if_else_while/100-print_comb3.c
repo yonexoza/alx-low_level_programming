60ccf68e775903d572211e14735ec6e0b3e5fd42
root <root@JONZI.JONZI>
2022-07-05 01:21:28 +0300

    prints two digit num

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stido.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	int x;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (x = 48; x <= 57; n++)
+		{
+			putchar(n);
+			putchar(x);
+			if (n != 56 || x != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
