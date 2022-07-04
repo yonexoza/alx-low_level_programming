12eb8b80d37656dee588234680458ef7bbb9f5b5
root <root@JONZI.JONZI>
2022-07-05 00:30:36 +0300

    prints num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
