9229ef684ece002d1c18f19410f2c1eafeeb7fb2
root <root@JONZI.JONZI>
2022-07-05 00:41:48 +0300

    prints hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
