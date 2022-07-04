a606c7c314653d715841508f915c5303afa332df
root <root@JONZI.JONZI>
2022-07-05 01:42:42 +0300

    prints three digit num

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
 
+int main(void)
+{
+	int n;
+	int x;
+	int y;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (x = 49; x <= 57; x++)
+		{
+			for (y = 50; y <= 57; y++)
+			{
+				if (x > n && y > x)
+				{
+					putchar(n);
+					putchar(x);
+					putchar(y);
+					if (n != 55 || x != 56 || y != 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
