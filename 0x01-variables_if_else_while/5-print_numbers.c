220d448bf5633cbbbda829bbb9557515e150fa8d
root <root@JONZI.JONZI>
2022-07-05 00:23:53 +0300

    prints num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n)
+	}
+	putchar(10);
+	return (0);
+}
