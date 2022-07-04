1e9fc99381797beb4f8deaf17d287ec4ca9abb21
root <root@JONZI.JONZI>
2022-07-05 00:49:03 +0300

    prints num with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	return (0);
+}
+
