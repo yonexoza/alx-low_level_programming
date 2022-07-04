e08aed6832224112897168a296423bb0c2712802
root <root@JONZI.JONZI>
2022-07-05 00:34:36 +0300

    prints lowercase alphabet reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
