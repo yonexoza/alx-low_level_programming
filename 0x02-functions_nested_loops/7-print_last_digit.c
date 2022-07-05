d8b35792acb5d7309be0536a2defc9b101660255
root <root@JONZI.JONZI>
2022-07-06 00:23:42 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,11 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
-	
+	int x;
+
 	if (n < 0)
-	n = -n;
-	last_digit = n % 10;
-	_putchar(last_digit + '0');
-	return (last_digit);
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
 }
