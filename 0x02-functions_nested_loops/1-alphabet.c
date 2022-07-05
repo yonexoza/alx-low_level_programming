cd2460aede269931999509fecd7dc7d5a9d30de0
root <root@JONZI.JONZI>
2022-07-05 22:32:43 +0300

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,10 @@
  * main - check the code
  * Return: always 0
  */
-int n;
-void print_alphabet(void);
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void)
 {
+	int n;
 	for (n = 97; n <= 122; n++)
 	{
 		_putchar(n);
