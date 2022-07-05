b0c94cbc35492f6327253494a1874a707df745db
root <root@JONZI.JONZI>
2022-07-05 05:30:41 +0300

    prints two two digit num

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,10 +6,25 @@
 
 int main(void)
 {
+	int n;
 	int x;
-	int y;
 
-
-		putchar(10);
+	for (n = 48; n <= 57; n++)
+	{
+		for (x = 49; x <= 57; x++)
+		{
+			if (x > n)
+			{
+				putchar(n);
+				putchar(x);
+				if (n != 56 || x != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
 	return (0);
 }
