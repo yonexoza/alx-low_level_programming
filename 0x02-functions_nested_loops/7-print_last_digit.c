e27119c884c1e5302bb0d7eefa2a162daf4d8ebe
root <root@JONZI.JONZI>
2022-07-06 00:01:19 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit -> absolute value
+ * Return: always 0
+ * @n: variable
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = %n;
+	_putchar(last_digit);
+	return (n);
+}
