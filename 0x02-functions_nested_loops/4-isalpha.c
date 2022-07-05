5749271d658a16e374f9aa0554a6adc248465ea6
root <root@JONZI.JONZI>
2022-07-05 23:36:41 +0300

    checks lowercase and upercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _islower -> hecks for lowercase character
+ * _isalpha -> hecks for lowercase character
  * Return: 1 and 0
  * @c: argument
  */
 
 int _isalpha(int c)
 {
-	return (c >= 'A' && c <= 'Z' && c >= 'a' && c <= 'z');
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
