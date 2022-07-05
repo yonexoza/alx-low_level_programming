34f195a628b3480dae57abaf5c5b7ab0c6d7e64a
root <root@JONZI.JONZI>
2022-07-05 05:27:38 +0300

    prints two two digit num

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,21 +9,7 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 0; y <= 99; y++)
-	{
-		for (x = y; x <= 99; x++)
-		{
-			if (x != y)
-			{
-				putchar((y/10)+48);
-				putchar((y%10)+48);
-				putchar(32);
-				putchar((x/10)+48);
-				putchar((x%10)+48);
-				if ((y*100)+x != 9899)
-				{
-					putchar(44);
-					putchar(32);
+
 		putchar(10);
 	return (0);
 }
