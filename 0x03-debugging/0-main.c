e2226df101ad3dbc887b1d9ef9b939f35820cfe8
root <root@JONZI.JONZI>
2022-07-07 22:00:54 +0300

    debug posetive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,36 +1,16 @@
-
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - starting point
- * @i: vairable
- * Return: always 0
- */
-void postitive_or_negative(int i)
-{
-	int n;
-
-	srand(time(0));
-	n = i;
-	if (n > 0)
-		printf("%d is positve ", n);
-	if (n == 0)
-		printf("%d is zero ", n);
-	if (n < 0)
-	{
-		printf("%d is negative ", n);
-	}
-	return (i);
-}
+ * * main - tests function that prints
+ * * if integer is positive or negative
+ * * Return: 0
+ * */
 
 int main(void)
 {
 	int i;
 
 	i = 0;
-	postitive_or_negative(i);
-
+	positive_or_negative(i);
 	return (0);
 }
