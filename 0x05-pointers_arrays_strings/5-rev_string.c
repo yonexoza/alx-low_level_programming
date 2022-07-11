ed9c8507b6e535ad1534485215c8bd9e8f315ab2
root <root@JONZI.JONZI>
2022-07-11 21:55:51 +0300

    revers string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: character pointer
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int k;
+	char temp;
+
+	k = _strlen(s);
+	j = 0;
+
+	for (i = k; i > k / 2; i--)
+	{
+		_putchar(i + 48);
+		_putchar(' ');
+		temp1 = s[i];
+		s[i] = s[j++];
+		s[j] = temp;
+
+	}
+}
+
+/**
+ * _strlen - length of the string
+ * @s: string
+ * Return: return counter
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+	int i;
+
+	counter = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	return (counter);
+}
