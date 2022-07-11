8e3aa6758a0b4ee09045f6698bbad2db953b5913
root <root@JONZI.JONZI>
2022-07-11 21:41:38 +0300

    prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: character pointer
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
