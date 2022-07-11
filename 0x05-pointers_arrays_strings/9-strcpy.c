2de65b933a615a6e606f04663537f03b35b43cbb
root <root@JONZI.JONZI>
2022-07-11 22:17:46 +0300

    copies string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @src: source for the copy
+ * @dest: destionation for the copy
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	len = _strlen(src);
+	for (i = 0; i <= j; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - length of the string
+ * @s: string
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+	int i;
+
+	counter = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	return (counter);
+}
