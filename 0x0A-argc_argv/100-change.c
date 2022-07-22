4424335f7e537635f2ad53cec9998e0dc2658e84
root <root@JONZI.JONZI>
2022-07-22 22:41:28 +0300

    advance

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,72 +2,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
+* main - prints the minumin number of coins for perfect change
+* @argc: number of arguments
+* @argv: the vector that hold the arguments
+*
+* Description: using arc and argv to achieve the intended function
+* Return: returns 0 if it works or something else if error
+* A: if argc does not have only 2 arguments or if the
+*	second argument is not a digit
+* B: turn the whole number into an int, then subtract
+*	25, 10, 5, 2, and 1. till no more. increase counter and return it
+* C: checks if any non digits in the string.
+*/
+
+int main(int argc, char **argv)
 {
+	int number;
 	int count = 0;
 
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-			else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
- */
-int main(int argc, char *argv[])
-{
-	int i, coin;
-
-	coin = 0;
-
-	if (argc != 2)
+	if (argc != 2)/* A */
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-
-	else
+	number = atoi(argv[1]);
+	while (number > 0)
 	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
+		while (number >= 25)
+		{
+			number -= 25;
+			count++;
+		}
+		while (number >= 10)
+		{
+			number -= 10;
+			count++;
+		}
+		while (number >= 5)
+		{
+			number -= 5;
+			count++;
+		}
+		while (number >= 2)
+		{
+			number -= 2;
+			count++;
+		}
+		while (number >= 1)
+		{
+			number -= 1;
+			count++;
+		}
 	}
-
+	printf("%d\n", count);
 	return (0);
 }
